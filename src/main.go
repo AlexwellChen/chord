4b779c78770318da9d51d796590551cec358ed0e
Alexwell <935562189@qq.com>
2022-12-25 16:47:43 +0100

    Change dir

--- src/main.go
+++ src/main.go
@@ -6,14 +6,14 @@ import (
 	"os"
 	"strings"
 
-	"github.com/AlexwellChen/chord/utils"
+	"github.com/AlexwellChen/chord"
 )
 
 func main() {
 	// Parse command line arguments
-	Arguments := utils.GetCmdArgs()
+	Arguments := chord.GetCmdArgs()
 	fmt.Println(Arguments)
-	node := utils.StartChord(Arguments)
+	node := chord.StartChord(Arguments)
 	// Get user input for printing states
 	reader := bufio.NewReader(os.Stdin)
 	for {
@@ -28,24 +28,24 @@ func main() {
 			key, _ := reader.ReadString('\n')
 			key = strings.TrimSpace(key)
 			fmt.Println(key)
-			resultAddr, err := utils.ClientLookUp(key, node)
+			resultAddr, err := chord.ClientLookUp(key, node)
 			if err != nil {
 				fmt.Print(err)
 			} else {
 				fmt.Println("The address of the key is ", resultAddr)
 
 			}
-			
+
 			// Check if the key is stored in the node
-			checkFileExistRPCReply := utils.CheckFileExistRPCReply{}
-			err = utils.ChordCall(resultAddr, "Node.CheckFileExistRPC", key, &checkFileExistRPCReply)
+			checkFileExistRPCReply := chord.CheckFileExistRPCReply{}
+			err = chord.ChordCall(resultAddr, "Node.CheckFileExistRPC", key, &checkFileExistRPCReply)
 			if err != nil {
 				fmt.Println("Check file exist RPC call failed")
 			} else {
 				if checkFileExistRPCReply.Exist {
 					// Get the address of the node that stores the file
-					var getNameRPCReply utils.GetNameRPCReply
-					err = utils.ChordCall(resultAddr, "Node.GetNameRPC", "", &getNameRPCReply)
+					var getNameRPCReply chord.GetNameRPCReply
+					err = chord.ChordCall(resultAddr, "Node.GetNameRPC", "", &getNameRPCReply)
 					if err != nil {
 						fmt.Println("Get name RPC call failed")
 					} else {
@@ -59,7 +59,7 @@ func main() {
 			fmt.Println("Please enter the file name you want to store")
 			fileName, _ := reader.ReadString('\n')
 			fileName = strings.TrimSpace(fileName)
-			err := utils.ClientStoreFile(fileName, node)
+			err := chord.ClientStoreFile(fileName, node)
 			if err != nil {
 				fmt.Print(err)
 			} else {
@@ -75,7 +75,7 @@ func main() {
 			fmt.Println("Please enter the file name you want to get")
 			fileName, _ := reader.ReadString('\n')
 			fileName = strings.TrimSpace(fileName)
-			err := utils.ClientGetFile(fileName, node)
+			err := chord.ClientGetFile(fileName, node)
 			if err != nil {
 				fmt.Println(err)
 			} else {
